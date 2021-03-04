8086a4fdbdfa1512fbd742b26a6b82f34cb98d76
parkgun6 <a01088807068@gmail.com>
2021-03-04 19:57:09 +0900

    히카리cp, 마이바티스, 시간테스트

--- 0303/src/test/java/org/zerock/dao/TimeMapperTests.java
+++ 0303/src/test/java/org/zerock/dao/TimeMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.zerock.mapper.TimeMapper;
 
 import lombok.extern.log4j.Log4j;
