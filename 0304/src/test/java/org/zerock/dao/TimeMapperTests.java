c7752cc74fbbf4495fc381e3c19fea22840e3cb6
parkgun6 <a01088807068@gmail.com>
2021-03-04 20:03:20 +0900

    Tests

---
+++ 0304/src/test/java/org/zerock/dao/TimeMapperTests.java
@@ -0,0 +1,33 @@
+package org.zerock.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.zerock.mapper.TimeMapper;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class TimeMapperTests {
+
+	@Autowired
+	TimeMapper timeMapper;
+	
+	@Test
+	public void testGetTime2() {
+		log.info(timeMapper.getTime2());
+	}
+	
+	@Test
+	public void testTime() {
+		
+		log.info(timeMapper);
+		
+		log.info(timeMapper.getTime());
+	}
+}
