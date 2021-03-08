af4edb15a757fe9bc15043b3d2294f15f31ec132
parkgun6 <a01088807068@gmail.com>
2021-03-08 19:34:07 +0900

    config 파일 테스트

---
+++ j0308/src/test/java/org/zerock/main/common/ConfigLoadTests.java
@@ -0,0 +1,44 @@
+package org.zerock.main.common;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.common.config.CommonConfig;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {CommonConfig.class})
+@Log4j
+public class ConfigLoadTests {
+
+	
+	@Autowired
+	private DataSource ds;
+	
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+	
+	//jdbc 연결 테스트
+	@Test
+	public void test1() {
+		log.info("test1.............");
+		assertNotNull(ds);
+	}
+	
+	@Test
+	public void testSession() {
+		SqlSession session = sqlSessionFactory.openSession();
+		log.info(session);
+		
+		session.close();
+	}
+}
