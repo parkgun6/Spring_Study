aced9ad88c99af97222f2ece5003ec394a0ccbf3
parkgun6 <a01088807068@gmail.com>
2021-03-09 20:32:33 +0900

    DTO to VO

---
+++ j0309_4/src/main/java/org/zerock/board/service/BoardServiceImpl.java
@@ -0,0 +1,63 @@
+package org.zerock.board.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.zerock.board.domain.Board;
+import org.zerock.board.dto.BoardDTO;
+import org.zerock.board.mapper.BoardMapper;
+import org.zerock.common.dto.PageDTO;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+//자동주입을 하면 코드가 깔끔해진다
+@Service
+@Log4j
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+	private final BoardMapper mapper;
+	
+	
+	
+	//인터페이스에 있는 default메서드를 활용한 코드
+	//vo를 사용했다.
+	@Override
+	public void register(BoardDTO boardDTO) {
+		
+		//인터페이스에 있는 toDomain을 활용해서 DTO를 VO로 변환해주었다.
+		Board vo = toDomain(boardDTO);
+		log.info(vo);
+		
+		mapper.insert(vo);
+		
+	}
+	
+	//인터페이스에 있는 default메서드를 활용하지 않은 코드
+	@Override
+	public List<BoardDTO> getPageList(PageDTO pageDTO) {
+
+		return mapper.getList(pageDTO.getSkip(), pageDTO.getPerSheet())
+		.stream().map(board -> {
+			BoardDTO dto = new BoardDTO();
+			dto.setBno(board.getBno());
+			dto.setTitle(board.getTitle());
+			dto.setContent(board.getContent());
+			dto.setWriter(board.getWriter());
+			dto.setRegDate(board.getRegDate());
+			dto.setUpdatedate(board.getUpdatedate());
+			return dto;
+		}).collect(Collectors.toList());
+	}
+
+	@Override
+	public int getTotalCount() {
+		//이 사이에 아무것도 안들어갔지만 나중에 AOP할때 부가적인 기능이 추가된다
+		return mapper.getTotalCount();
+	}
+
+	
+
+}
