2b1ca82ac3384432dfa9423864fa626bbd5d5ba3
parkgun6 <a01088807068@gmail.com>
2021-03-15 16:38:50 +0900

    Transaction Test

---
+++ aop/src/main/java/org/zerock/service/SampleService.java
@@ -0,0 +1,11 @@
+package org.zerock.service;
+
+public interface SampleService {
+
+	public String doA();
+	
+	public String doB();
+	
+	public String doC();
+	
+}
---
+++ aop/src/main/java/org/zerock/service/SampleServiceImpl.java
@@ -0,0 +1,53 @@
+package org.zerock.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.mapper.SampleMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+@RequiredArgsConstructor
+public class SampleServiceImpl implements SampleService {
+
+	private final SampleMapper mapper;
+	
+	@Transactional
+	@Override
+	public String doA() {
+		log.info("doA.............");
+		
+		String str = "동해물과 백두산이 마르고 닳도록 하느님이 보우하사"
+				+ "우리나라만세 무궁화 삼천리 화려강산 대한사람 대한으로 길이 보전하세";
+
+		mapper.insert1(str);
+		mapper.insert2(str);
+		
+		return "doA";
+	}
+
+	@Override
+	public String doB() {
+		log.info("doB.............");
+		
+		mapper.insert3("제목...", "내용...", "user00");
+		
+		log.info("-----------------------------");
+		log.info("-----------------------------");
+		log.info(mapper.getLast());
+		log.info("-----------------------------");
+		
+		return "doB";
+	}
+
+	@Override
+	public String doC() {
+		log.info("doC.............");
+		return "doC";
+	}
+
+	
+	
+}
